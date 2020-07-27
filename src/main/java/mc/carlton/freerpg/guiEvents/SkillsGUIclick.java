e6990c0cabd0c6c4274d24c9f97074618a3433db
Jack Carlton <customjack@gmail.com>
2020-07-27 04:24:20 -0400

    New command system, /frpg [...] Added new command, /frpg help Fixed problem with leaderboard command

--- src/main/java/mc/carlton/freerpg/guiEvents/SkillsGUIclick.java
+++ src/main/java/mc/carlton/freerpg/guiEvents/SkillsGUIclick.java
@@ -57,12 +57,12 @@ public class SkillsGUIclick implements Listener {
                     switch (e.getCurrentItem().getType()) {
                         case ARROW:
                             p.closeInventory();
-                            p.performCommand("skills");
+                            p.performCommand("frpg skills");
                             break;
                         case COMPOSTER:
                             if ((int) pStatAll.get("global").get(20) >= 250 && (int) pStatAll.get("global").get(9) > 0) {
                                 p.closeInventory();
-                                p.performCommand("confirmationGUI " + skillName);
+                                p.performCommand("frpg confirmationGUI " + skillName);
                             }
                             else {
                                 if ((int) pStatAll.get("global").get(9) > 0) {
@@ -143,7 +143,7 @@ public class SkillsGUIclick implements Listener {
                             } else {
                                 p.sendMessage(ChatColor.RED + "You do not have any skill tokens.");
                             }
-                            p.performCommand("skillTreeGUI " + skillName);
+                            p.performCommand("frpg skillTreeGUI " + skillName);
                             break;
 
                         case GREEN_TERRACOTTA:
@@ -160,7 +160,7 @@ public class SkillsGUIclick implements Listener {
                                 p.sendMessage(ChatColor.RED + "You do not have any passive tokens.");
                             }
 
-                            p.performCommand("skillTreeGUI " + skillName);
+                            p.performCommand("frpg skillTreeGUI " + skillName);
                             break;
                         case GREEN_DYE:
                             int maxLevel2 = passiveMax.findMaxLevel(skillName,2);
@@ -178,7 +178,7 @@ public class SkillsGUIclick implements Listener {
                                     p.sendMessage(ChatColor.RED + "You do not have any passive tokens.");
                                 }
                             }
-                            p.performCommand("skillTreeGUI " + skillName);
+                            p.performCommand("frpg skillTreeGUI " + skillName);
                             break;
                         case BLUE_DYE:
                             int maxLevel3 = passiveMax.findMaxLevel(skillName,3);
@@ -196,53 +196,53 @@ public class SkillsGUIclick implements Listener {
                                     p.sendMessage(ChatColor.RED + "You do not have any passive tokens.");
                                 }
                             }
-                            p.performCommand("skillTreeGUI " + skillName);
+                            p.performCommand("frpg skillTreeGUI " + skillName);
                             break;
                         case LIME_WOOL:
                         case GRAY_WOOL:
                             switch (skillName) {
                                 case "digging":
                                     if ((int) pStats.get(11) > 0 && e.getSlot() == 47) {
-                                        p.performCommand("flintToggle");
-                                        p.performCommand("skillTreeGUI " + skillName);
+                                        p.performCommand("frpg flintToggle");
+                                        p.performCommand("frpg skillTreeGUI " + skillName);
                                     }
                                     else if ((int) pStats.get(13) > 0  && e.getSlot() == 48 ) {
-                                        p.performCommand("megaDigToggle");
-                                        p.performCommand("skillTreeGUI " + skillName);
+                                        p.performCommand("frpg megaDigToggle");
+                                        p.performCommand("frpg skillTreeGUI " + skillName);
                                     }
                                     break;
                                 case "mining":
                                     if ((int) pStats.get(11) > 0) {
-                                        p.performCommand("veinMinerToggle");
-                                        p.performCommand("skillTreeGUI " + skillName);
+                                        p.performCommand("frpg veinMinerToggle");
+                                        p.performCommand("frpg skillTreeGUI " + skillName);
                                     }
                                     break;
                                 case "fishing":
                                     if ((int) pStats.get(11) > 0  && e.getSlot() == 47) {
-                                        p.performCommand("grappleToggle");
-                                        p.performCommand("skillTreeGUI " + skillName);
+                                        p.performCommand("frpg grappleToggle");
+                                        p.performCommand("frpg skillTreeGUI " + skillName);
                                     }
                                     else if ((int) pStats.get(12) > 0  && e.getSlot() == 48) {
-                                        p.performCommand("hotRodToggle");
-                                        p.performCommand("skillTreeGUI " + skillName);
+                                        p.performCommand("frpg hotRodToggle");
+                                        p.performCommand("frpg skillTreeGUI " + skillName);
                                     }
                                     break;
                                 case "agility":
                                     if ((int) pStats.get(13) > 0) {
-                                        p.performCommand("speedTogggle");
-                                        p.performCommand("skillTreeGUI " + skillName);
+                                        p.performCommand("frpg speedTogggle");
+                                        p.performCommand("frpg skillTreeGUI " + skillName);
                                     }
                                     break;
                                 case "alchemy":
                                     if ((int) pStats.get(13) > 0) {
-                                        p.performCommand("potionToggle");
-                                        p.performCommand("skillTreeGUI " + skillName);
+                                        p.performCommand("frpg potionToggle");
+                                        p.performCommand("frpg skillTreeGUI " + skillName);
                                     }
                                     break;
                                 case "smelting":
                                     if ((int) pStats.get(13) > 0) {
-                                        p.performCommand("flamePickToggle");
-                                        p.performCommand("skillTreeGUI " + skillName);
+                                        p.performCommand("frpg flamePickToggle");
+                                        p.performCommand("frpg skillTreeGUI " + skillName);
                                     }
                                     break;
                                 default:
@@ -256,87 +256,87 @@ public class SkillsGUIclick implements Listener {
                             switch (craftingName) {
                                 case "Cow Egg":
                                     p.closeInventory();
-                                    p.performCommand("craftingGUI " + "farming1");
+                                    p.performCommand("frpg craftingGUI " + "farming1");
                                     break;
                                 case "Bee Egg":
                                     p.closeInventory();
-                                    p.performCommand("craftingGUI " + "farming2");
+                                    p.performCommand("frpg craftingGUI " + "farming2");
                                     break;
                                 case "Mooshroom Egg":
                                     p.closeInventory();
-                                    p.performCommand("craftingGUI " + "farming3");
+                                    p.performCommand("frpg craftingGUI " + "farming3");
                                     break;
                                 case "Horse Egg":
                                     p.closeInventory();
-                                    p.performCommand("craftingGUI " + "farming4");
+                                    p.performCommand("frpg craftingGUI " + "farming4");
                                     break;
                                 case "Slime Egg":
                                     p.closeInventory();
-                                    p.performCommand("craftingGUI " + "farming5");
+                                    p.performCommand("frpg craftingGUI " + "farming5");
                                     break;
                                 case "Dragonless Arrows":
                                     p.closeInventory();
-                                    p.performCommand("craftingGUI " + "archery1");
+                                    p.performCommand("frpg craftingGUI " + "archery1");
                                     break;
                                 case "Power I Book":
                                     p.closeInventory();
-                                    p.performCommand("craftingGUI " + "enchanting1");
+                                    p.performCommand("frpg craftingGUI " + "enchanting1");
                                     break;
                                 case "Efficiency I Book":
                                     p.closeInventory();
-                                    p.performCommand("craftingGUI " + "enchanting2");
+                                    p.performCommand("frpg craftingGUI " + "enchanting2");
                                     break;
                                 case "Sharpness I Book":
                                     p.closeInventory();
-                                    p.performCommand("craftingGUI " + "enchanting3");
+                                    p.performCommand("frpg craftingGUI " + "enchanting3");
                                     break;
                                 case "Protection I Book":
                                     p.closeInventory();
-                                    p.performCommand("craftingGUI " + "enchanting4");
+                                    p.performCommand("frpg craftingGUI " + "enchanting4");
                                     break;
                                 case "Luck of the Sea I Book":
                                     p.closeInventory();
-                                    p.performCommand("craftingGUI " + "enchanting5");
+                                    p.performCommand("frpg craftingGUI " + "enchanting5");
                                     break;
                                 case "Lure I Book":
                                     p.closeInventory();
-                                    p.performCommand("craftingGUI " + "enchanting6");
+                                    p.performCommand("frpg craftingGUI " + "enchanting6");
                                     break;
                                 case "Frost Walker I Book":
                                     p.closeInventory();
-                                    p.performCommand("craftingGUI " + "enchanting7");
+                                    p.performCommand("frpg craftingGUI " + "enchanting7");
                                     break;
                                 case "Depth Strider I Book":
                                     p.closeInventory();
-                                    p.performCommand("craftingGUI " + "enchanting8");
+                                    p.performCommand("frpg craftingGUI " + "enchanting8");
                                     break;
                                 case "Mending Book":
                                     p.closeInventory();
-                                    p.performCommand("craftingGUI " + "enchanting9");
+                                    p.performCommand("frpg craftingGUI " + "enchanting9");
                                     break;
                                 case "Fortune I Book":
                                     p.closeInventory();
-                                    p.performCommand("craftingGUI " + "enchanting10");
+                                    p.performCommand("frpg craftingGUI " + "enchanting10");
                                     break;
                                 case "Water Breathing Potion":
                                     p.closeInventory();
-                                    p.performCommand("craftingGUI " + "alchemy1");
+                                    p.performCommand("frpg craftingGUI " + "alchemy1");
                                     break;
                                 case "Speed Potion":
                                     p.closeInventory();
-                                    p.performCommand("craftingGUI " + "alchemy2");
+                                    p.performCommand("frpg craftingGUI " + "alchemy2");
                                     break;
                                 case "Fire Resistance Potion":
                                     p.closeInventory();
-                                    p.performCommand("craftingGUI " + "alchemy3");
+                                    p.performCommand("frpg craftingGUI " + "alchemy3");
                                     break;
                                 case "Healing Potion":
                                     p.closeInventory();
-                                    p.performCommand("craftingGUI " + "alchemy4");
+                                    p.performCommand("frpg craftingGUI " + "alchemy4");
                                     break;
                                 case "Strength Potion":
                                     p.closeInventory();
-                                    p.performCommand("craftingGUI " + "alchemy5");
+                                    p.performCommand("frpg craftingGUI " + "alchemy5");
                                     break;
                             }
                         default:
@@ -365,7 +365,7 @@ public class SkillsGUIclick implements Listener {
                     switch (e.getCurrentItem().getType()) {
                         case ARROW:
                             p.closeInventory();
-                            p.performCommand("skills");
+                            p.performCommand("frpg skills");
                             break;
                         case RED_TERRACOTTA:
                             if (e.getSlot() == 3 || e.getSlot() == 21 || e.getSlot() == 39) {
@@ -430,7 +430,7 @@ public class SkillsGUIclick implements Listener {
                             } else {
                                 p.sendMessage(ChatColor.RED + "You do not have any skill tokens.");
                             }
-                            p.performCommand("skillTreeGUI " + skillName);
+                            p.performCommand("frpg skillTreeGUI " + skillName);
                             break;
 
                         case GREEN_TERRACOTTA:
